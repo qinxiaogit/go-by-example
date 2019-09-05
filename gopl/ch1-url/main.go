e8a51caffd185d7f6afe600f1640d21f18406640
qinxiao <346243025@qq.com>
2019-09-05 18:23:08 +0800

    gou 圣经

---
+++ gopl/ch1-url/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main(){
+	start:=time.Now()
+	ch:=make(chan string)
+	for _,url:=range os.Args[1:]{
+		go fetch(url,ch)
+		//res,err:=http.Get(url)
+		//if err!=nil{
+		//	panic(err)
+		//}
+		//b,err2:=ioutil.ReadAll(res.Body)
+		//if err2!=nil{
+		//	panic(err2)
+		//}
+		//res.Body.Close()
+
+		//fmt.Println(res.StatusCode)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string,ch chan <-string){
+	start:=time.Now()
+	resp,err:=http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+	nbytes,err2:= io.Copy(ioutil.Discard,resp.Body)
+	if err != nil {
+		ch <- fmt.Sprint(err2) // send to channel ch
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
