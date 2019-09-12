8a7f551c04d2e53aafcbca03a028b1a2c0d40d99
qinxiao <346243025@qq.com>
2019-09-12 18:12:25 +0800

    中间件

--- blog/routers/router.go
+++ blog/routers/router.go
@@ -2,24 +2,33 @@ package routers
 
 import (
 	"blog/pkg/setting"
+	"blog/routers/api"
 	v1 "blog/routers/api/v1"
 	"github.com/gin-gonic/gin"
+	"blog/middleware/jwt"
+	"net/http"
 )
 
 func InitRouter()*gin.Engine{
 	r:=gin.New()
+	r.LoadHTMLGlob("views/**/*")
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
 	gin.SetMode(setting.RunModel)
+	r.GET("/auth",api.GetAuth)
 
 	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200,gin.H{
-			"php":"echp 'hello world'",
+		//c.JSON(200,gin.H{
+		//	"php":"echp 'hello world'",
+		//})
+		c.HTML(http.StatusOK,"index.tmpl",gin.H{
+			"title":"main website",
 		})
 	})
+	apiV1 := r.Group("/api/v1")
+	apiV1.Use(jwt.JWT())
 	{
-		apiV1 := r.Group("/api/v1")
 		apiV1.GET("/tags", v1.GetTags)
 		apiV1.POST("/tags", v1.AddTag)
 		apiV1.PUT("/tags/:id", v1.EditTag)
